166a11bd86b71475c6bea5b6cb28353fca03803a
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-04-03 16:52:30 -0600

    feat: first route (book)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 //package dotenv for use environment variables from .env file
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 const cors = require("cors");
+
+//import routes
+const bookRoutes = require("./routes/books.routes");
 
 const app = express();
 
 //avoid compatibility problems with browsers
 app.use(cors());
+
+app.use(morgan("dev"));
+
+app.use(bookRoutes);
 
 const port = process.env.PORT;
 
