33838a9d97475b58445619c1ff9d4db19dc37f4f
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-04-03 23:55:42 -0600

    feat: configuren route books

---
+++ src/postgresql.js
@@ -0,0 +1,7 @@
+const { Pool } = require("pg");
+new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.HOST,
+    database:
+});
--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -2,8 +2,39 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("Hola mundo");
+/**
+ * Get list of books
+ */
+router.get("/books", (req, res) => {
+    res.send("Books");
+});
+
+/**
+ * Get a single book
+ */
+router.get("/book/:id", (req, res) => {
+    res.send("Book");
+});
+
+/**
+ * Register new book
+ */
+router.post("/books", (req, res) => {
+    res.send("Save a book");
+});
+
+/**
+ * Delete one book
+ */
+router.delete("/book/:id", (req, res) => {
+    res.send("Delete one book");
+});
+
+/**
+ * Update one book
+ */
+router.put("/book/:id", (req, res) => {
+    res.send("Update one book");
 });
 
 module.exports = router;
