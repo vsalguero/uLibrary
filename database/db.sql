7138adc3bd5c694802c77bc96e1241c84e5f4007
Vladimir Salguero <vladimir891@hotmail.com>
2022-04-05 17:04:47 -0600

    feat: creating database

--- database/db.sql
+++ database/db.sql
@@ -1,6 +1,9 @@
 CREATE DATABASE librarybd
 
 CREATE TABLE books(
-    id SERIAL
-    title
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    author VARCHAR(255),
+    publish_year SMALLINT,
+    genre VARCHAR(100)
 )
--- src/postgresql.js
+++ src/postgresql.js
@@ -3,5 +3,5 @@ new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.HOST,
-    database:
+    database: process.env.DB
 });
