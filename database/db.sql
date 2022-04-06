f21fe818e66a944fa11d6cf3b924b3b045341f7a
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-04-06 00:12:32 -0600

    feat: trying the database connection

--- database/db.sql
+++ database/db.sql
@@ -1,9 +1,35 @@
-CREATE DATABASE librarybd
+CREATE DATABASE LibraryDB;
 
 CREATE TABLE books(
     id SERIAL PRIMARY KEY,
-    title VARCHAR(255),
-    author VARCHAR(255),
-    publish_year SMALLINT,
-    genre VARCHAR(100)
-)
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    publish_year SMALLINT NOT NULL,
+    genre VARCHAR(100) NOT NULL,
+    created_at DATE NOT NULL DEFAULT CURRENT_DATE,
+    updated_at DATE NOT NULL DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(55) NOT NULL,
+    last_name VARCHAR(55) NOT NULL,
+    email VARCHAR(60) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    role VARCHAR(20),
+    created_at DATE NOT NULL DEFAULT CURRENT_DATE,
+    updated_at DATE NOT NULL DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE loans (
+   id SERIAL PRIMARY KEY,                  
+   id_user INT NOT NULL,                
+   id_book INT NOT NULL,
+   FOREIGN KEY (id_book)                          
+   REFERENCES books (id)                  
+   ON DELETE CASCADE,
+   FOREIGN KEY (id_user)
+   REFERENCES users (id)
+   ON DELETE CASCADE
+);
+
