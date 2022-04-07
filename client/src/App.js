a218bad272b52d14df834ee662c7049fd7946f57
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-04-07 00:08:26 -0600

    feat: adding material ui elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Container } from '@mui/material';
+import BookForm from './components/bookform';
 import BookList from './components/booklist';
+import Menu from './components/navbar';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<BookList />} />
-      </Routes>
+      <Menu />
+      <Container>
+        <Routes>
+          <Route path='/' element={<BookList />} />
+          <Route path='/book/new' element={<BookForm />} />
+        </Routes>
+      </Container>
 
     </BrowserRouter>
   );
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { AppBar, Box, Toolbar, Typography, Container, Button } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
+    const navigate = useNavigate();
     return (
-        <div>
-            Navigation
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" color="transparent">
+                <Container>
+                    <Toolbar>
+                        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                            <Link style={{ textDecoration: "none", color: "#eee" }} to="/">
+                                uLibrary
+                            </Link>
+                        </Typography>
+                        <Button variant="contained" onClick={() => navigate("book/new")}>
+                            New Book
+                        </Button>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </Box >
     );
 }
 
