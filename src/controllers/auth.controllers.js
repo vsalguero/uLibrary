4de5faad828165c862c7f0d558d0871b5fb85e81
Vladimir Salguero <vladimir891@hotmail.com>
2022-04-19 09:00:33 -0600

    feat: create login token

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -13,13 +13,15 @@ const registerController = async (req, res) => {
       });
     } else {
 
-        const passwordHash = await encrypt(req.body.password);
+        const passwordHash = await encrypt(password);
         
-        const dataUser = pool.query(
-          `INSERT INTO users (first_name, last_name, email, password, role) VALUES ('${first_name}', '${last_name}', '${email}', '${passwordHash}', '${role}');`);
+        const dataUser = await pool.query(
+          `INSERT INTO users (first_name, last_name, email, password, role) VALUES ('${first_name}', '${last_name}', '${email}', '${passwordHash}', '${role}') 
+          RETURNING id, first_name, last_name, email, role;`);
+          console.log(dataUser);
         const data = {
-            token: await tokenSign(dataUser),
-            user: dataUser
+            token: await tokenSign(dataUser.rows),
+            user: dataUser.rows
         }
         res.send({ data });
     }
@@ -32,19 +34,20 @@ const loginController = async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await pool.query(`select * from users where email = '${email}';`);
-        if (!user) {
+        if (user.rows.length < 1) {
             handleHttpErrors(res, "USER_NOT_EXISTS", 404);
             return
         }
-        const hashPassword = user.password;
+        const userData = user.rows[0];
+        const hashPassword = userData.password;
         const check = await compare(password, hashPassword);
         if (!check) {
             handleHttpErrors(res, "PASSWORD_INCORRECT", 401);
             return
         }
         const data = {
-            token: await tokenSign(user),
-            user
+            token: await tokenSign(userData),
+            userData
         }
         res.send(data);
 
@@ -53,5 +56,36 @@ const loginController = async (req, res) => {
     }
 
 };
+
+/*const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const result = await pool.query(`select * from users where email = '${email}';`);
+      if (result.rows.length > 0) {
+      const user = result.rows[0];
+    
+      const validPassword = await bcryptjs.compare(password, user.password);
+      if (validPassword) {
+        res.send({
+          token: 'success'
+        });
+      } else {
+        res.send({
+          token: 'Incorrect password'
+        });
+      }
+    } else {
+      res.send({
+        token: 'The username not exists'
+      });
+    }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        error: "Database error while Login!" + err, //Database connection error
+      });
+    }
+  };*/
+  
 
 module.exports = { registerController, loginController};
