9eeb31d7c0c16b9bbd223a108e37e21093e288da
Vladimir Salguero <vladimir891@hotmail.com>
2022-04-20 14:29:14 -0600

    feat: logout route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -12,4 +12,13 @@ router.post("/register", registerController);
 
 router.post("/login", loginController);
 
+router.get('/logout', function(req, res, next) {
+  // remove the req.user property and clear the login session
+  req.logout();
+  // destroy session data
+  req.session = null;
+  // redirect to homepage
+  res.redirect('/');
+});
+
 module.exports = router;
