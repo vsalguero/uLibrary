e6ba34cabb1250f629dc894721b9e8bfa1f7f8ba
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-04-21 22:23:46 -0600

    feat: add proxy in client package.json

--- src/index.js
+++ src/index.js
@@ -55,6 +55,7 @@ app.get("*", (req, res) =>{
 });
 
 const port = process.env.PORT;
+//app.listen(process.env.PORT || 4000);
 
 app.listen(port, () => {
   console.log(`App listen http://localhost:${port}`);
