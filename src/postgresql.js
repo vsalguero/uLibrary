97274329e9e252af36e04eb6e840ee208f9bcad8
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-04-22 22:05:07 -0600

    fix: columns in mobile screen

--- src/postgresql.js
+++ src/postgresql.js
@@ -1,19 +1,19 @@
 const { Pool } = require("pg");
 //LOCAL CONFIGURATION
-const db = new Pool({
+/*const db = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB
-});
+});*/
 
 //PG Server in heroku using DATABASE_URL
-/*const db = new Pool({
+const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
-});*/
+});
 
 module.exports = db;
